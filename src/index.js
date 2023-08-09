622c7466e6cd323a46d0d1cd9997a9dedce2946a
WavyBoyJodii <najeecasey@gmail.com>
2023-08-09 16:48:21 -0400

    style song section with transitions and javascript lib

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+import ScrollReveal from 'scrollreveal';
+
+const menu = document.getElementById('menu-icon');
+const navlist = document.querySelector('.navlist');
+
+const sr = ScrollReveal({
+  distance: '70px',
+  duration: 2700,
+  reset: true,
+});
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+sr.reveal('.left-hero', { delay: 200, origin: 'bottom' });
+sr.reveal('.right-hero', { delay: 350, origin: 'top' });
