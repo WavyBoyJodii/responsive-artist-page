d11cb0c980bb1aef2f677463d2b2ae57cc9583e7
WavyBoyJodii <najeecasey@gmail.com>
2023-08-12 13:07:06 -0400

    style bts section make responsive add popup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import ScrollReveal from 'scrollreveal'; // eslint-disable-line import/no-extraneous-dependencies
+// import shyEmbrace from './assets/shy-embrace.JPG';
 
 const menu = document.getElementById('menu-icon');
 const navlist = document.querySelector('.navlist');
@@ -17,3 +18,14 @@ menu.onclick = () => {
 
 sr.reveal('.left-hero', { delay: 200, origin: 'bottom' });
 sr.reveal('.right-hero', { delay: 350, origin: 'top' });
+
+document.querySelectorAll('.media-element img').forEach((pic) => {
+  pic.onclick = () => {
+    document.querySelector('.popup-bts').style.display = 'block';
+    document.querySelector('.popup-bts img').src = pic.getAttribute('src');
+  };
+});
+
+document.querySelector('.popup-bts span').onclick = () => {
+  document.querySelector('.popup-bts').style.display = 'none';
+};
